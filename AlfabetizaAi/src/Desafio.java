b801bc0907ae2beaa7eadbe995b1b7670511f7bc
Filipe Prata <eusouf.prata@gmail.com>
2024-01-03 20:47:57 -0300

    feat: adição de método Criar na classe Desafio

--- AlfabetizaAi/src/Desafio.java
+++ AlfabetizaAi/src/Desafio.java
@@ -1,16 +1,49 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Desafio {
 
     private String titulo;
     private char tipo; //(v - video, e - escrito)
     private Professor autor;
-    private boolean foiAprovado;
+/*
+    public Desafio() {}
 
     public Desafio(String titulo, char tipo, Professor autor) {
         this.titulo = titulo;
         this.tipo = tipo;
         this.autor = autor;
-        this.foiAprovado = false;
     }
+
+    // CREATE
+
+    Professor luis = new Professor("Luis", 22, "klsdjnckj");
+    Professor filipe = new Professor("Filipe", 32, "ppodomspm");
+
+    ArrayList<Professor> professores = new ArrayList<>();
+*/
+    public void criarDesafio() {
+
+        System.out.println("\nDigite o título do desafio: ");
+        Scanner input = new Scanner(System.in);
+        this.titulo = input.nextLine();
+
+        System.out.println("Digite o tipo do desafio: (V - Vídeo ou E - Escrito)");
+        this.tipo = input.next().charAt(0);
+        input.nextLine();
+
+        professores.add(luis);
+        professores.add(filipe);
+
+        System.out.println("Selecione um entre os seguintes professores: ");
+        for (int i = 0; i < professores.size(); i++) {
+            System.out.println((i + 1) + " - " + professores.get(i).getNome());
+        }
+        int prof = input.nextInt();
+        this.autor = professores.get(prof-1);
+
+    }
+
 
     public boolean exibirDesafio(){
         System.out.println("Exibindo desafio.");
@@ -41,11 +74,9 @@ public class Desafio {
         this.autor = autor;
     }
 
-    public boolean isFoiAprovado() {
-        return foiAprovado;
+    public static void main(String[] args) {
+        Desafio um = new Desafio();
+        um.criarDesafio();
     }
 
-    public void setFoiAprovado(boolean foiAprovado) {
-        this.foiAprovado = foiAprovado;
-    }
 }
