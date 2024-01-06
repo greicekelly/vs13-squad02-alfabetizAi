de9a066579515a4ebd588a16b947fffb06714e95
Filipe Prata <eusouf.prata@gmail.com>
2024-01-06 00:20:20 -0300

    feat: finalizando menu do aluno, iniciando menu do professor

--- AlfabetizaAi/src/MenuNumerico.java
+++ AlfabetizaAi/src/MenuNumerico.java
@@ -11,6 +11,7 @@ public class MenuNumerico {
         AlunoService listaAluno = new AlunoService();
         AdminService listaAdmin = new AdminService();
         ProfessorService listaProfessor = new ProfessorService();
+        ModuloService modulosConcluidos = new ModuloService();
 
         listaAluno.adicionarAluno(new Aluno("Lucas", LocalDate.parse("2018-07-01"), "lucas@email.com", new ArrayList<>()));
 
@@ -18,8 +19,48 @@ public class MenuNumerico {
         String dataNascimento;
         String email;
         LocalDate data;
+        int opcaoMenu;
+
+        private static void exibirMenuInicial() {
+            System.out.println("\n---- Menu ----");
+            System.out.println("1. Pressione 1 se você é um Aluno");
+            System.out.println("2. Pressione 2 se você é um Professor");
+            System.out.println("3. Pressione 3 se você é um Admin");
+            System.out.println("0. Sair");
+        }
+
+        private static void menuAluno() {
+            System.out.println("\n---- Menu ----");
+            System.out.println("1. Pressione 1 para editar seus dados cadastrais"); //UPDATE
+            System.out.println("2. Pressione 2 para acessar os módulos"); // READ
+            System.out.println("3. Pressione 3 para acessar os desafios"); // READ
+            System.out.println("4. Pressione 4 para acessar os módulos concluídos"); //READ
+            System.out.println("0. Voltar");
+
+        }
+
+        private static void menuProfessor() {
+            System.out.println("\n---- Menu ----");
+            System.out.println("1. Pressione 1 para editar seus dados cadastrais"); //UPDATE
+            System.out.println("2. Pressione 2 para criar um novo módulo"); // CREATE //passar por aprovação novamente
+            System.out.println("3. Pressione 3 para modificar um módulo"); // UPDATE //passar por aprovação novamente
+            System.out.println("4. Pressione 4 para criar um novo desafio"); // CREATE //passar por aprovação novamente
+            System.out.println("5. Pressione 5 para modificar um desafio"); // UPDATE //passar por aprovação novamente
+            System.out.println("0. Voltar");
 
 
+        }
+
+        private static void menuAdmin() {
+            System.out.println("\n---- Menu ----");
+            System.out.println("1. Pressione 1 para fazer o login");  // READ
+            System.out.println("2. Pressione 2 para fazer o cadastro"); // CREATE
+            System.out.println("3. Pressione 3 para editar seus dados cadastrais"); //UPDATE
+            System.out.println("4. Pressione 4 para aprovar um módulo"); // CREATE
+            System.out.println("5. Pressione 5 para deletar um módulo"); // DELETE
+            System.out.println("0. Voltar");
+
+        }
 
         while (true) {
             exibirMenuInicial();
@@ -33,7 +74,7 @@ public class MenuNumerico {
                     case 1:
                         System.out.println("Você escolheu a opção Aluno:");
                         System.out.println("1 - Login / 2 - Cadastrar / 0 - Voltar");
-                        int opcaoMenu = sc.nextInt();
+                        opcaoMenu = sc.nextInt();
                         sc.nextLine();
 
                             switch (opcaoMenu){
@@ -55,15 +96,15 @@ public class MenuNumerico {
 
                                         switch (escolhaInterna) {
                                             case 1:
-                                                System.out.println("Informe seu email: ");
+                                                System.out.println("Informe o novo email: ");
                                                 String alunoASerEditado = sc.nextLine();
                                                 Aluno aluno = listaAluno.consultarAlunoEmail(alunoASerEditado);
-                                                System.out.println("Informe o seu nome: ");
+                                                System.out.println("Informe o novo nome: ");
                                                 nome = sc.nextLine();
-                                                System.out.println("Informe sua data de nascimento: yyyy-mm-dd");
+                                                System.out.println("Informe a nova data de nascimento (yyyy-mm-dd): ");
                                                 dataNascimento = sc.nextLine();
                                                 LocalDate dataNova = LocalDate.parse(dataNascimento);
-                                                System.out.println("Informe o seu email: ");
+                                                System.out.println("Informe o novo email: ");
                                                 email = sc.nextLine();
                                                 listaAluno.editarAluno(aluno, new Aluno(nome, dataNova, email));
                                                 System.out.println("Aluno editado com sucesso");
@@ -85,9 +126,11 @@ public class MenuNumerico {
                                                 DesafiosDoModulo.visualizarDesafios();
                                                 DesafiosDoModulo.consultarDesafio(sc.nextInt());
                                                 System.out.println("Parabéns por concluir o desafio!");
+                                                modulosConcluidos.adicionar(moduloEscolhidoParaDesafios);
 
                                                 break;
                                             case 4:
+                                                modulosConcluidos.visualizarTodos();
                                                 break;
                                             default:
                                                 System.out.println("Opção inválida. Tente novamente.");
@@ -96,29 +139,100 @@ public class MenuNumerico {
                                     }
                                 case 2:
                                     System.out.println("Informe o seu nome: ");
-                                    String nomeCadastro = sc.nextLine();
+                                    nome = sc.nextLine();
                                     System.out.println("Informe sua data de nascimento: yyyy-mm-dd");
-                                    String dataNascimentoCadastro = sc.nextLine();
-                                    data = LocalDate.parse(dataNascimentoCadastro);
+                                    dataNascimento = sc.nextLine();
+                                    data = LocalDate.parse(dataNascimento);
                                     System.out.println("Informe o seu email: ");
                                     email = sc.nextLine();
-                                    Aluno alunoCadastrado = new Aluno(nomeCadastro, data, email);
+                                    Aluno alunoCadastrado = new Aluno(nome, data, email);
                                     listaAluno.adicionarAluno(alunoCadastrado);
                                     System.out.println("Aluno cadastrado com sucesso");
                                     break;
 
                                 case 0:
                                     break;
+
                                 default:
                                     System.out.println("Opção inválida.");
                                     break;
                         }
                     case 2:
-                        System.out.println("Você escolheu a opção Professor.");
-                        // Adicione o código correspondente à opção 2 aqui
-                        break;
+                        System.out.println("Você escolheu a opção Professor:");
+                        System.out.println("1 - Login / 2 - Cadastrar / 0 - Voltar");
+                        opcaoMenu = sc.nextInt();
+                        sc.nextLine();
+
+                        switch (opcaoMenu){
+
+                            case 1:
+                                System.out.println("Informe seu email: ");
+                                email = sc.nextLine();
+                                System.out.println("Informe sua data de nascimento: yyyy-mm-dd");
+                                dataNascimento = sc.nextLine();
+                                data = LocalDate.parse(dataNascimento);
+                                Professor professorLogado = listaProfessor.loginProfessor(email, data);
+                                if (professorLogado == null) {
+                                    System.out.println("Falha no login. Verifique suas credenciais.");
+                                    break;
+                                } else {
+                                    System.out.println("Login bem-sucedido! Bem-vindo, " + professorLogado.getNome() + ".");
+                                    menuProfessor();
+                                    int escolhaInterna = sc.nextInt();
+                                    sc.nextLine();
+
+                                    switch (escolhaInterna) {
+
+                                        case 1:
+                                            System.out.println("Informe o novo email: ");
+                                            String professorASerEditado = sc.nextLine();
+                                            Professor professor = listaProfessor.consultarProfessorEmail(professorASerEditado);
+                                            System.out.println("Informe o novo nome: ");
+                                            nome = sc.nextLine();
+                                            System.out.println("Informe a nova data de nascimento (yyyy-mm-dd): ");
+                                            dataNascimento = sc.nextLine();
+                                            LocalDate dataNova = LocalDate.parse(dataNascimento);
+                                            System.out.println("Informe o novo email: ");
+                                            email = sc.nextLine();
+                                            listaProfessor.editar(professor, new Professor(nome, dataNova, email));
+                                            System.out.println("Professor editado com sucesso");
+                                            break;
+
+                                        case 2:
+                                            break;
+
+                                        case 3:
+                                            break;
+
+                                        case 4:
+                                            break;
+
+                                    }
+                                }
+
+                            case 2:
+                                System.out.println("Informe o seu nome: ");
+                                nome = sc.nextLine();
+                                System.out.println("Informe sua data de nascimento: yyyy-mm-dd");
+                                dataNascimento = sc.nextLine();
+                                data = LocalDate.parse(dataNascimento);
+                                System.out.println("Informe o seu email: ");
+                                email = sc.nextLine();
+                                Professor professorCadastrado = new Professor(nome, data, email);
+                                listaProfessor.adicionar(professorCadastrado);
+                                System.out.println("Professor cadastrado com sucesso");
+                                break;
+
+                            case 0:
+                                break;
+
+                            default:
+                                System.out.println("Opção inválida.");
+                                break;
+                        }
+
                     case 3:
-                        System.out.println("Você escolheu a opção 3.");
+                        System.out.println("Você escolheu a opção Admin.");
                         // Adicione o código correspondente à opção 3 aqui
                         break;
                     case 0:
@@ -127,55 +241,13 @@ public class MenuNumerico {
                         System.exit(0);
                     default:
                         System.out.println("Opção inválida. Tente novamente.");
-                }
-            } else {
+                } else {
                 System.out.println("Por favor, insira um número válido.");
                 sc.next();
             }
         }
     }
 
-    private static void exibirMenuInicial() {
-        System.out.println("\n---- Menu ----");
-        System.out.println("1. Pressione 1 se você é um Aluno");
-        System.out.println("2. Pressione 2 se você é um Professor");
-        System.out.println("3. Pressione 3 se você é um Admin");
-        System.out.println("0. Sair");
-    }
 
-    private static void menuAluno() {
-        System.out.println("\n---- Menu ----");
-        System.out.println("1. Pressione 1 para editar seus dados cadastrais"); //UPDATE
-        System.out.println("2. Pressione 2 para acessar os módulos"); // READ
-        System.out.println("3. Pressione 3 para acessar os desafios"); // READ
-        System.out.println("4. Pressione 4 para acessar os módulos concluídos"); //READ
-        System.out.println("0. Voltar");
-
-    }
-
-    private static void menuProfessor() {
-        System.out.println("\n---- Menu ----");
-        System.out.println("1. Pressione 1 para fazer o login");  // READ
-        System.out.println("2. Pressione 2 para fazer o cadastro"); // CREATE
-        System.out.println("3. Pressione 3 para editar seus dados cadastrais"); //UPDATE
-        System.out.println("4. Pressione 4 para criar um novo módulo"); // CREATE //passar por aprovação novamente
-        System.out.println("5. Pressione 5 para modificar um módulo"); // UPDATE //passar por aprovação novamente
-        System.out.println("4. Pressione 6 para criar um novo desafio"); // CREATE //passar por aprovação novamente
-        System.out.println("5. Pressione 7 para modificar um desafio"); // UPDATE //passar por aprovação novamente
-        System.out.println("0. Voltar");
-
-
-    }
-
-    private static void menuAdmin() {
-        System.out.println("\n---- Menu ----");
-        System.out.println("1. Pressione 1 para fazer o login");  // READ
-        System.out.println("2. Pressione 2 para fazer o cadastro"); // CREATE
-        System.out.println("3. Pressione 3 para editar seus dados cadastrais"); //UPDATE
-        System.out.println("4. Pressione 4 para aprovar um módulo"); // CREATE
-        System.out.println("5. Pressione 5 para deletar um módulo"); // DELETE
-        System.out.println("0. Voltar");
-
-    }
 
 }
--- AlfabetizaAi/src/ProfessorService.java
+++ AlfabetizaAi/src/ProfessorService.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +17,25 @@ public class ProfessorService {
         return professores;
     }
 
+    public Professor loginProfessor(String email, LocalDate dataNascimento) {
+        for (Professor professor : professores) {
+            professor.getDataDeNascimento();
+            if (professor.getEmail().equals(email) && professor.getDataDeNascimento().equals(dataNascimento)) {
+                return professor;
+            }
+        }
+        return null;
+    }
+
+    public Professor consultarProfessorEmail(String email) {
+        for (Professor professor : professores) {
+            if(professor.getEmail().equals(email)){
+                return professor;
+            }
+        }
+        return null;
+    }
+
     public void visualizar() {
         for(int i = 0; i < professores.size(); i++) {
             System.out.println("Id do Professor: " + i + "\n" + professores.get(i) + "\n--------");
@@ -27,12 +47,10 @@ public class ProfessorService {
         System.out.println("Nome: " + professor.getNome() + " - Idade: " + professor.getDataDeNascimento() + " - E-mail: " + professor.getEmail());
     }
 
-    public void editar(int i, Professor professorEditado) {
-        Professor professor = professores.get(i);
+    public void editar(Professor professor, Professor professorEditado) {
         professor.setNome(professorEditado.getNome());
         professor.setDataDeNascimento(professorEditado.getDataDeNascimento());
         professor.setEmail(professorEditado.getEmail());
-        System.out.println("Edição realizada com sucesso");
     }
 
     public void remover(int i) {
