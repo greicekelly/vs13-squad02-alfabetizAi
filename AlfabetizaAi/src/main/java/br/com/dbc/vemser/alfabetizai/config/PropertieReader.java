c479c648ddd01088ed26a42197b4cf5eeeaa61ea
Tiago Raupp <tiraupp@gmail.com>
2024-01-24 11:00:21 -0300

    feat-inclusao configuracoes email service admin

---
+++ AlfabetizaAi/src/main/java/br/com/dbc/vemser/alfabetizai/config/PropertieReader.java
@@ -0,0 +1,20 @@
+package br.com.dbc.vemser.alfabetizai.config;
+
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class PropertieReader {
+    @Value("${ambiente}")
+    String ambiente;
+
+    @Value("${spring.application.name}")
+    private String app;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+}
