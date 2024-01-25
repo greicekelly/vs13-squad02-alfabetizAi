ed091bd67893ecc53e95926b8d5f082fbce32c16
shaienne23 <shaienneo@gmail.com>
2024-01-24 23:43:41 -0300

    feat: open Api - Desafio

--- AlfabetizaAi/src/main/java/br/com/dbc/vemser/alfabetizai/dto/DesafioCreateDTO.java
+++ AlfabetizaAi/src/main/java/br/com/dbc/vemser/alfabetizai/dto/DesafioCreateDTO.java
@@ -1,6 +1,7 @@
 package br.com.dbc.vemser.alfabetizai.dto;
 
 import br.com.dbc.vemser.alfabetizai.enums.TipoDesafio;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,17 +16,22 @@ import javax.validation.constraints.NotNull;
 public class DesafioCreateDTO {
 
     @NotNull(message = "Id não pode ser nulo.")
+    @Schema(description = "Numero do id Desafio", required = true, example = "3")
     private int id;
 
     @NotNull(message = "Id do Modulo não pode ser nulo.")
+    @Schema(description = "Numero do id do Modulo", required = true, example = "3")
     private int idModulo;
 
     @NotBlank(message = "O titulo não pode estar em branco ou ser Nulo!")
+    @Schema(description = "Nome do titulo do desafio", required = true, example = "Animais com a letra A")
     private String titulo;
 
     @NotBlank(message = "O conteudo não pode estar em branco ou ser Nulo!")
+    @Schema(description = "Descrição do conteudo do desafio", required = true, example = "Insira nome de animais com a letra A")
     private String conteudo;
 
     @NotNull(message = "O Tipo de desafio não pode ser Nulo!")
+    @Schema(description = "Tipo de Desafio", required = true, example = "QUIZ")
     private TipoDesafio tipoDesafio;
 }
